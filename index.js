a99bf9cd2cc70933c2e7deeeb5c380c21a3dea54
alouilouai <louaialoui1993@gmail.com>
2023-11-05 01:28:42 +0100

    refinement

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON request bodies
 app.use(bodyParser.json());
-
+// Middleware for logging
+app.use(morgan('dev'));
 // Import your routes
 const taskRouter = require('./src/routes/task.routes');
 
@@ -15,5 +17,5 @@ app.use('/api', taskRouter);
 
 // Start the Express server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
